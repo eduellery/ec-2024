ca321b3f95c70d3ae44c15fbda116b073c5083c2
Eduardo Ellery <eduellery@gmail.com>
2024-12-12 06:03:01 +0100

    Refactor into single cargo

---
+++ everybody_codes_2024/src/event2024/quest01.rs
@@ -0,0 +1,55 @@
+pub fn part1(content: &str) -> i32 {
+    let mut result = 0;
+    for ch in content.chars() {
+        result += get_monster_score(ch)
+    }
+    return result;
+}
+
+pub fn part2(content: &str) -> i32 {
+    let mut result = 0;
+    let mut chars = content.chars();
+    while let (Some(first), Some(second)) = (chars.next(), chars.next()) {
+        if first != 'x' && second != 'x' {
+            result += 2
+        }
+        result += get_monster_score(first);
+        result += get_monster_score(second);
+    }
+    if let Some(_) = chars.next() {
+        unimplemented!("Monster ungrouped")
+    }
+    return result;
+}
+
+pub fn part3(content: &str) -> i32 {
+    let mut result = 0;
+    let mut chars = content.chars();
+    while let (Some(first), Some(second), Some(third)) = (chars.next(), chars.next(), chars.next())
+    {
+        let under_test = [first, second, third];
+        let number_of_x = under_test.iter().filter(|&&ch| ch == 'x').count();
+        result += match number_of_x {
+            0 => 6,
+            1 => 2,
+            _ => 0,
+        };
+        result += get_monster_score(first);
+        result += get_monster_score(second);
+        result += get_monster_score(third);
+    }
+    if let Some(_) = chars.next() {
+        unimplemented!("Monster(s) ungrouped")
+    }
+    return result;
+}
+
+fn get_monster_score(monster: char) -> i32 {
+    match monster {
+        'A' | 'x' => 0,
+        'B' => 1,
+        'C' => 3,
+        'D' => 5,
+        _ => unimplemented!("Can't find monster {}", monster),
+    }
+}
---
+++ everybody_codes_2024/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod event2024 {
+    pub mod quest01;
+}
+
+pub mod util {
+    pub mod ansi;
+    pub mod integer;
+    pub mod parse;
+}
